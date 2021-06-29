08acc633ba6f620ed50d79ef90b2fe60a74e6e90
tw <987146971@qq.com>
2021-06-29 17:46:01 +0800

    '支持mac环境'

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-pyinstaller -Fw --noupx -i ./icon/2.ico --add-data "./ui/*:ui" --add-data "./con/*:icon" wfrpc.py
+pyinstaller -Fw --noupx -i ./icon/2.ico --add-data "./ui/*:ui" --add-data "./icon/*:icon" wfrpc.py
