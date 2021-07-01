a83c2c5e9b9379674ef963c26d7f3bbe2081f6dc
Gavin <gavin@GavindeMac.local>
2021-07-01 15:38:09 +0800

    支持mac环境

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-pyinstaller -Fw --noupx -i ./icon/2.ico --add-data "./ui/*:ui" --add-data "./icon/*:icon" wfrpc.py
+pyinstaller -Fw --noupx --clean -y -i ./icon/2.icns --add-data "./ui/*:ui" --add-data "./icon/*:icon" wfrpc.py
